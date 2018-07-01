99aeaabe8647bb0c7e3d0f09803021db4c1d901c
muhammadmmunir <muhammadmmunir24@gmail.com>
2018-07-01 15:37:34 +0700

    AlphaCut

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/GraphFuzzy.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class GraphFuzzy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartGraph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartGraph
+            // 
+            this.chartGraph.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chartGraph.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartGraph.Legends.Add(legend1);
+            this.chartGraph.Location = new System.Drawing.Point(151, 1);
+            this.chartGraph.Name = "chartGraph";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Blue;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Red;
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Color = System.Drawing.Color.Black;
+            series3.IsVisibleInLegend = false;
+            series3.Legend = "Legend1";
+            series3.Name = "dot1";
+            series4.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.Black;
+            series4.IsVisibleInLegend = false;
+            series4.Legend = "Legend1";
+            series4.Name = "dot2";
+            this.chartGraph.Series.Add(series1);
+            this.chartGraph.Series.Add(series2);
+            this.chartGraph.Series.Add(series3);
+            this.chartGraph.Series.Add(series4);
+            this.chartGraph.Size = new System.Drawing.Size(513, 335);
+            this.chartGraph.TabIndex = 0;
+            this.chartGraph.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(49, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(49, 213);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(307, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "coba";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // GraphFuzzy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 389);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chartGraph);
+            this.Name = "GraphFuzzy";
+            this.Text = "GraphFuzzy";
+            ((System.ComponentModel.ISupportInitialize)(this.chartGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartGraph;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/GraphFuzzy.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class GraphFuzzy : Form
+    {
+
+        public GraphFuzzy()
+        {
+            InitializeComponent();
+            label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chartGraph.Series)
+            {
+                series.Points.Clear();
+            }
+
+            chartGraph.ChartAreas[0].AxisX.CustomLabels.Clear();
+            chartGraph.ChartAreas[0].AxisY.CustomLabels.Clear();
+
+            int min = Convert.ToInt32(textBox1.Text);
+            int middle = Convert.ToInt32(textBox2.Text);
+            int max = Convert.ToInt32(textBox3.Text);
+
+            chartGraph.ChartAreas[0].AxisX.Maximum = max + 10;
+            chartGraph.ChartAreas[0].AxisX.Minimum = min - 10;
+
+            chartGraph.Series[0].Points.AddXY(min, 0);
+            chartGraph.Series[0].Points.AddXY(middle, 1);
+            chartGraph.Series[0].Points.AddXY(max, 0);
+
+            chartGraph.ChartAreas[0].AxisX.CustomLabels.Add(min-3, min+3, "" + min);
+            chartGraph.ChartAreas[0].AxisX.CustomLabels.Add(middle - 3, middle + 3, "" + middle);
+            chartGraph.ChartAreas[0].AxisX.CustomLabels.Add(max - 3, max + 3, "" + max);
+            chartGraph.ChartAreas[0].AxisY.CustomLabels.Add(1 - 0.1, 1 + 0.1, "" + 1);
+
+            if (textBox4.Text != "")
+            {
+                double x = Convert.ToDouble(textBox4.Text);
+                chartGraph.ChartAreas[0].AxisY.CustomLabels.Add(x-0.1, x+0.1, "" + x);
+
+                chartGraph.Series[1].Points.AddXY(min-10, x);
+                chartGraph.Series[1].Points.AddXY(max+10, x);
+
+                double posDot1 = x * (middle - min) + min;
+                double posDot2 = -1 * (x * (max - middle) - (max - middle) - middle);
+
+                chartGraph.Series[2].Points.AddXY(posDot1, 0);
+                chartGraph.Series[2].Points.AddXY(posDot1, x);
+                chartGraph.Series[3].Points.AddXY(posDot2, 0);
+                chartGraph.Series[3].Points.AddXY(posDot2, x);
+                chartGraph.ChartAreas[0].AxisX.CustomLabels.Add(posDot1 - 3, posDot1 + 3, "" + posDot1);
+                chartGraph.ChartAreas[0].AxisX.CustomLabels.Add(posDot2 - 3, posDot2 + 3, "" + posDot2);
+
+                label1.Text = "Interval [ " + posDot1 + ", " + posDot2 + " ]";
+               
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
